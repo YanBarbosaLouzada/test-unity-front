bf49019fc37ab151bedbaab5a81a2dc8227d938d
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2025-05-05 16:30:05 -0300

    react&unity

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const handleClick = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/api/cadastrar', {
+        msg: 'Olá backend!'
+      });
+      console.log(response.data);
+    } catch (err) {
+      console.error('Erro ao enviar:', err);
+    }
+  };
+
+  return <button onClick={handleClick}>Enviar para o backend</button>;
+
+}
+
+export default App
